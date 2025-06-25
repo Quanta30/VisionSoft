5ff2497818aacae47caf92d2db8a64bbd8c10d5b
Parag <paragpatkulkar123@gmail.com>
2025-06-25 16:14:14 +0530

    Quotation Detail 80percent Done

--- obj/Debug/net8.0/VisionSoft.AssemblyInfo.cs
+++ obj/Debug/net8.0/VisionSoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428c7889a8c7183c6f04aa6d534ba039dfc03a5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e7faf81bf798270b022183725b18362c6ae298")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
