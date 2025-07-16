25a40382933c9487c3f92f20d816fe678b3d51ce
Parag <paragpatkulkar123@gmail.com>
2025-07-16 13:31:55 +0530

    StockAdjustment updated

--- obj/Debug/net8.0/VisionSoft.AssemblyInfo.cs
+++ obj/Debug/net8.0/VisionSoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d9f44eb9dc56719b6caea4a74cf4825e874be9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67539b5c8d3421989293ab0ece29d31482a7806")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisionSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
